ebcf4e117bb7f263ffd76efd26779b0de5c42384
Mark Peters <mapeters101@earthlink.net>
2025-05-15 09:20:19 -0400

    Work on lesson117 to 6:20.

---
+++ lesson117.py
@@ -0,0 +1,20 @@
+
+class Edge:
+
+    def __init__(self, weight, start_vertex, target_vertex):
+        self.weight = weight
+        self.start_vertex = start_vertex
+        self.target_vertex = target_vertex
+
+class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.visited = False
+        # this is the node where we came from in the shortest path
+        self.predecessor = None
+        # this is how we store the children
+        self.adjacency_list = []
+        # this is the minimum distance (shortest path) from the source vertex (starting vertex)
+        self.min_distance = float('inf')
+
